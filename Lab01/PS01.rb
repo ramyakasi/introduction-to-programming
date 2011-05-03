afbb191280cebe2e0e1fe8af5ea7fa4b1fef1b0b
Ramya Kasiviswanathan <ramkvs@yahoo.com>
2011-05-02 20:50:46 -0400

    Finished PS01.

--- Lab01/PS01.rb
+++ Lab01/PS01.rb
@@ -2,5 +2,11 @@
 
 # Write a program that does the following in order:
 # 1. Asks the user to enter his/her first name.
+puts "Enter your First name:"
+firstname= gets.chomp
 # 2. Asks the user to enter his/her last name.
+puts "Enter your Last name:"
+lastname= gets.chomp
 # 3. Prints out the user's first and last name in a greeting.
+puts firstname + " " + lastname
+
